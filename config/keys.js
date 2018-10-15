dbcc9727ee15dbb03573e8018cc202da87624653
Akshay Gupta <akshay.gupta@arm.com>
2018-10-15 10:07:44 +0100

    Creating The JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://aksgup01:aksgup01@ds131743.mlab.com:31743/devconnector-1"
+  mongoURI:
+    "mongodb://aksgup01:aksgup01@ds131743.mlab.com:31743/devconnector-1",
+  secretOrKey: "secret"
 };
