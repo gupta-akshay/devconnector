35bb9765b971d74e7d291bbbcffa3e62e0984b01
Akshay Gupta <akshay.gupta@arm.com>
2018-10-23 10:29:35 +0100

    Deployment Ready

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://aksgup01:aksgup01@ds131743.mlab.com:31743/devconnector-1",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
